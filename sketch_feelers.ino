e9ebfcf2fe15e2440afd71d358b04527e4d288d5
stimpe <scott.timpe.300@my.csun.edu>
2017-10-24 10:42:42 -0700

    Added code to support both left and right feelers

--- sketch_feelers.ino
+++ sketch_feelers.ino
@@ -8,18 +8,26 @@
 
 #include <EEPROM.h>
 
-int LED = 2, FEELER = 3;
+int LED_L = 2, FEELER_L = 3;
+int LED_R = 4, FEELER_R = 5;
 
 // the setup routine runs once when you press reset:
 void setup() {
-  pinMode(LED, OUTPUT);
-  pinMode(FEELER, INPUT_PULLUP);  //uses internal pullup resistor
+  pinMode(LED_L, OUTPUT);
+  pinMode(LED_R, OUTPUT);
+  pinMode(FEELER_L, INPUT_PULLUP);  //uses internal pullup resistor
+  pinMode(FEELER_R, INPUT_PULLUP);  //uses internal pullup resistor
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  int feeler_contact = 0;
-  //active low signal from feeler
-  feeler_contact = not(digitalRead(FEELER));
-  digitalWrite(LED, feeler_contact);
+  int feeler_contact_L = 0;
+  int feeler_contact_R = 0;
+  //active low signal from feelers
+  //left feeler
+  feeler_contact_L = not(digitalRead(FEELER_L));
+  digitalWrite(LED_L, feeler_contact_L);
+  //right feeler
+  feeler_contact_R = not(digitalRead(FEELER_R));
+  digitalWrite(LED_R, feeler_contact_R);
 }
