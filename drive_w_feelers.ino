72c24379cb7a31af23c21079648a48b7e8ac82c6
Scotland <scott.timpe.300@my.csun.edu>
2017-10-27 18:41:33 -0700

    initial commit. ugly.

---
+++ drive_w_feelers.ino
@@ -0,0 +1,32 @@
+#include <MotorControl.h>
+#include <PinDeclarations.h>
+
+MotorControl Red = MotorControl();
+
+int feeler_contact_L = 0;
+int feeler_contact_R = 0;
+
+void setup() {
+  Serial.print("begin routine");
+  // set all the motor control pins to outputs  
+  pinMode(LeftFeeler, INPUT_PULLUP);
+  pinMode(RightFeeler, INPUT_PULLUP);
+
+  Red.motorForward(255);
+}
+
+void loop() {
+
+  //active low inputs
+  feeler_contact_L = not(digitalRead(LeftFeeler));
+  feeler_contact_R = not(digitalRead(RightFeeler));
+
+  if(feeler_contact_L || feeler_contact_R)
+  {
+    Red.motorOff();
+   }
+  else
+  {
+    Red.motorForward(255);
+  }
+}
